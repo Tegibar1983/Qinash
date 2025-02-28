1a9998bd0073b2cdda3e05776bd7effcd9544021
Tegibar1983 <tbtf1991@gmail.com>
2025-02-28 19:24:30 +0300

    Add nav bar

--- Qinash/core/views.py
+++ Qinash/core/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#index page views
 def index(request):
     return render(request, 'core/index.html')
+
+#contact page views
+def contact(request):
+    return render(request, 'core/contact.html')
