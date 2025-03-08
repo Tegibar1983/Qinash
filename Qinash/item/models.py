1f422174e913d89d94255fc3afb12c0d87fb390b
Tegibar1983 <tbtf1991@gmail.com>
2025-03-08 22:54:31 +0300

    models Commit

--- Qinash/item/models.py
+++ Qinash/item/models.py
@@ -1,9 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # data base model for item app categories
 
 class Category(models.Model):
-    #models goes here!
+    #Category database model
+    name=models.CharField(max_length=255)
 
 
+    class Meta:
+        ordering=('name',)
+        #change categors to Categories
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        # change arbitrary name definition to acctual object naming"""
+        return self.name
+    
+class Item(models.Model):
+    #Items DB model
+    category=models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
+    name=models.CharField(max_length=255)
+    description=models.TextField(blank=True, null=True)
+    price=models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold=models.BooleanField(default=False)
+    created_by=models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # change arbitrary name definition to acctual object naming"""
+        return self.name
 
