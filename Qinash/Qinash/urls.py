48d3d31bd268fed17ac13b5be30aad281bec1f99
Tegibar1983 <tbtf1991@gmail.com>
2025-03-15 15:50:06 +0300

    items page

--- Qinash/Qinash/urls.py
+++ Qinash/Qinash/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from core.views import index, contact
 
@@ -9,4 +11,4 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
     
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
