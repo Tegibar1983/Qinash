a6063cc602e31f6be7bf4055777f9fddceb453be
Tegibar1983 <tbtf1991@gmail.com>
2025-03-18 16:59:42 +0300

    create sign up forms

--- Qinash/core/views.py
+++ Qinash/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignupForm
 
 # Create your views here.
 
@@ -15,3 +16,10 @@ def index(request):
 #contact page views
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form':form
+    })
